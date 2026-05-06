d359138f6c482814bb1eef29a11b74540fd01dc0
çağlar <caglartezcan48@gmail.com>
2026-05-06 10:45:56 +0300

    fix: Header bilesenine use client eklendi ve build hatasi giderildi

--- components/fethiye/header.tsx
+++ components/fethiye/header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useEffect, useState } from "react"
 import { Menu, X, Compass, User, Building2, Sparkles } from "lucide-react"
 import { Button } from "@/components/ui/button"
